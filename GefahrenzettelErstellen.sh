21f23fd2c80d3c3685439f67789fc7c126dbaab9
tuxmaster <frank@bart.netz-von-frank>
2014-05-31 17:59:13 +0200

    - rcc wird von der richtigen Qt Version genommen.

--- GefahrenzettelErstellen.sh
+++ GefahrenzettelErstellen.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 ./GefahrenzettelDBerstellen.sh
-rcc -binary Gefahrenzettel.qrc -o Gefahrenzettel.rcc
+if [ -e /usr/bin/rcc-qt5 ]; then
+	echo Nehme Qt5
+	rcc-qt5 -binary Gefahrenzettel.qrc -o Gefahrenzettel.rcc
+else
+	echo Nehme Qt4
+	rcc -binary Gefahrenzettel.qrc -o Gefahrenzettel.rcc
+fi
