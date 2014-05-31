5f29343aaceef6414465cf4b58ca858a554e327f
tuxmaster <frank-buettner@gmx.net>
2014-05-31 18:35:51 +0200

    Die Meldung über die Qt Version wird nicht mehr benötigt.

--- GefahrenzettelErstellen.sh
+++ GefahrenzettelErstellen.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 ./GefahrenzettelDBerstellen.sh
 if [ -e /usr/bin/rcc-qt5 ]; then
-	echo Nehme Qt5
 	rcc-qt5 -binary Gefahrenzettel.qrc -o Gefahrenzettel.rcc
 else
-	echo Nehme Qt4
 	rcc -binary Gefahrenzettel.qrc -o Gefahrenzettel.rcc
 fi
